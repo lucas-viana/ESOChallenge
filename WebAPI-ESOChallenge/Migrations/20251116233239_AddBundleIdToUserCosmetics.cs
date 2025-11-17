f7d26733b34e7bac424d40319797ce6fe22bf9d0
lucas-viana <lucas_vianasilva@hotmail.com>
2025-11-16 22:28:16 -0300

    feat: adicionar campo BundleId em UserCosmetics para rastrear compras de bundle

---
+++ WebAPI-ESOChallenge/Migrations/20251116233239_AddBundleIdToUserCosmetics.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI_ESOChallenge.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBundleIdToUserCosmetics : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BundleId",
+                table: "UserCosmetics",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCosmetics_BundleId",
+                table: "UserCosmetics",
+                column: "BundleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserCosmetics_BundleId",
+                table: "UserCosmetics");
+
+            migrationBuilder.DropColumn(
+                name: "BundleId",
+                table: "UserCosmetics");
+        }
+    }
+}
